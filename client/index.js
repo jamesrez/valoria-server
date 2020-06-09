f30b8f7c9b39e4a635b5abcd255ed416fc79d24f
James Rezendes <jamesrezendes@Jamess-MacBook-Pro.local>
2020-06-09 14:40:54 -0700

    Creating and Verifying users

---
+++ client/index.js
@@ -0,0 +1,28 @@
+let provider;
+let signer;
+const valoria = new Valoria('/')
+
+async function start(){
+  if(!web3 || !web3.currentProvider) return;
+  provider = new ethers.providers.Web3Provider(web3.currentProvider);
+  signer = await provider.getSigner();
+}
+
+start();
+
+async function joinWithWeb3(){
+  const usernameSignature = await signer.provider.send('personal_sign', [
+    "valoria",
+    await signer.getAddress(),
+  ]);
+  const passwordSignature = await signer.provider.send('personal_sign', [
+    usernameSignature,
+    await signer.getAddress(),
+  ]);
+  valoria.register(usernameSignature, passwordSignature);
+}
+
+$('.valoriaJoinWithWeb3Wallet').on('click', () => {
+  joinWithWeb3();
+})
+
--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-var express= require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var bodyParser = require('body-parser');
-var crypto = require('crypto');
+
+const express= require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const bodyParser = require('body-parser');
+const { Crypto } = require("node-webcrypto-ossl");
+const crypto = new Crypto();
+const util = require('util');
 require('dotenv').config();
 
-app.get('/', (req, res, next) => res.send("Hi! I'm the Valoria Server"));
+app.set('views', 'client')
+app.set('view engine', 'pug');
+app.use(express.json())
+app.use(express.static('client'));
+app.get('/', (req, res) => res.render('index.pug'));
 
 const port = process.env.PORT || 80;
 
-var fs = require('fs');
+const fs = require('fs');
 const AWS = require('aws-sdk');
-var s3 = null;
-var data = null
+const s3 = null;
+let data = {};
 
-if(process.env.DEVELOPMENT){
+if(!process.env.AWS_ACCESS_KEY_ID){
   data = require('./data/data.json');
   data.online = {};
   saveData(data, () => {
     startSocketIO();
   });
-}else{
+} else {
   AWS.config.update({region: 'us-west-1'});
   s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -29,22 +36,23 @@ if(process.env.DEVELOPMENT){
   });
   s3.getObject({Bucket : process.env.S3_BUCKET, Key : "data.json"}, function(err, fileData) {
     if(err) return;
-    data = JSON.parse(fileData.Body.toString())
+    data = JSON.parse(fileData.Body.toString());
     data.online = {};
     data.peers = {};
     saveData(data, () => {
-      startSocketIO()
+      startSocketIO();
     });
-  })
+  });
 }
 
 function saveData(data, cb) {
-  if(process.env.DEVELOPMENT){
+  if(!process.env.AWS_ACCESS_KEY_ID){
     fs.writeFile('./data/data.json', JSON.stringify(data, null, 2), function (err) {
       if (err) return console.log(err);
       if(cb && typeof cb == 'function') cb();
     });
-  }else{
+  }else {
+    console.log("UPLOAD DATA: ", data);
     s3.upload({Bucket : "valoria", Key : "data.json", Body : JSON.stringify(data, null, 2)}, (err, fileData) => {
       if (err) console.error(`Upload Error ${err}`);
       if(cb && typeof cb == 'function') cb();
@@ -56,21 +64,23 @@ server.listen(port, () => {
   console.log("Listening on Port " + port);
 });
 
-app.set('views', './client')
+app.set('views', './client');
 app.set('view engine', 'pug');
-app.use(express.json())
+app.use(express.json());
 app.use(express.static('client'));
-app.use(bodyParser.json())//json parser
-app.use(bodyParser.urlencoded({ extended: true }))//urlencoded parser
+app.use(bodyParser.json());//json parser
+app.use(bodyParser.urlencoded({ extended: true }));//urlencoded parser
 
 app.get('/', (req, res) => {
   res.render('index.pug');
-})
+});
 
-async function startSocketIO() {
+function base64ToArrayBuffer(dataUrl, cb) {  
+  return Uint8Array.from(atob(dataUrl), c => c.charCodeAt(0))
+}
 
+function startSocketIO(){
   io.on('connection', function (socket) {
-
     socket.on('Create User', (d) => {
       if(!data.users[d.username]){
         data.users[d.username] = {
@@ -78,29 +88,58 @@ async function startSocketIO() {
           peers : {},
           sockets : {},
           name : d.username,
-          wrapped : d.wrapped,
+          eKey : d.eKey,
           keyPair : d.keyPair,
-        }
-        data.users[d.username].peers[d.peerId] = d.peerId
+        };
+        data.users[d.username].peers[d.peerId] = d.peerId;
         data.users[d.username].sockets[socket.id] = socket.id;
         data.online[socket.id] = {
           username : d.username,
           peerId : d.peerId
-        }
+        };
         saveData(data, () => {
           socket.emit("Create User", {success : true, ...d});
-        })
-      }else{
+        });
+      }else {
         socket.emit("Create User", {...d, err : "User already Exists"});
       }
-    })
+    });
   
   
     socket.on('Get User', (d) => {
       if(data.users[d.username]){
         socket.emit("Get User", data.users[d.username]);
+      }else {
+        socket.emit("Get User", {...d, err : "User Does Not Exist"});
+      }
+    });
+
+    socket.on('Login User', async (d) => {
+      if(data.users[d.username]){
+        const publicKey = await crypto.subtle.importKey(
+          "jwk", 
+          JSON.parse(data.users[d.username].keyPair.publicKey), {
+          name: "ECDSA",
+          namedCurve: "P-384"
+        }, true, ['verify']);
+        d.encoded = Uint8Array.from(Object.values(d.encoded));
+        const isUser = await crypto.subtle.verify({
+          name: "ECDSA",
+          hash: {name: "SHA-384"},
+        }, publicKey, d.signature, d.encoded);
+        if(isUser){
+          data.users[d.username].peers[d.peerId] = d.peerId;
+          data.users[d.username].sockets[socket.id] = socket.id;
+          data.online[socket.id] = {
+            username : d.username,
+            peerId : d.peerId
+          };
+          saveData(data, () => {
+            socket.emit("Login User", {success : true, ...d});
+          });
+        }
       }else{
-        socket.emit("Get User", {...d, err : "User Does Not Exist"})
+        socket.emit("Login User", {...d, err : "User Does Not Exist"});
       }
     })
 
@@ -114,9 +153,9 @@ async function startSocketIO() {
         }
         delete data.peers[peerId];
         delete data.online[socket.id];
-        saveData(data)
+        saveData(data);
       }
-    })
+    });
+  })
+};
 
-  });
-}
