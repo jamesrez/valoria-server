e93c7d0e85ee4ef2a5c0c443e8c06682f8452d60
James Rezendes <jamesrezendes@Jamess-MacBook-Pro.local>
2020-09-10 14:54:43 -0700

    Connecting to online user bug

--- client/index.js
+++ client/index.js
@@ -112,6 +112,7 @@ async function loadOnlineUsers(){
 
 async function connectToPeer(peer){
   valoria.getUser(peer.userId, (u) => {
+    console.log(u);
     window.thisPeer = u;
     delete currentChat.channel;
     currentChat.userId = u.id;
