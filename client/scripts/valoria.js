978fb76f011fa4de87bbaa82edaa0e48a0d06dab
James Rezendes <jamesrezendes@Jamess-MacBook-Pro.local>
2020-09-29 21:55:47 -0700

    p2p debug

--- client/scripts/valoria.js
+++ client/scripts/valoria.js
@@ -259,6 +259,9 @@
 
       if(opts.server) {
         this.primaryServer = opts.server;
+        if(opts.server.startsWith("/")){
+          opts.server = window.location.origin + opts.server;
+        }
         this.sockets[this.primaryServer] = io(this.primaryServer)
         this.setupPrimaryConnections(this.sockets[this.primaryServer]);
       } else {
